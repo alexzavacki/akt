1bb18f1ca236fa684358258609688c3df37dec3a
Alex Zavacki <alex.zavacki@gmail.com>
2011-03-06 02:08:46 +0200

    depends() rewritten

--- lib/Akt.php
+++ lib/Akt.php
@@ -59,34 +59,6 @@ class Akt
         $client = new $className($options);
         return $client->dispatch();
     }
-
-    /**
-     * Loads module by its name and instantiate it
-     *
-     * By default instantiate each module once (Singleton)
-     * If you need new instance of some module specify $instance parameter
-     *
-     * @param string $name
-     * @param string $instance
-     * @return object
-     */
-    public static function module($name, $instance = 'default')
-    {
-        return $name ? $name : $instance;
-    }
-
-    /**
-     * Register an alias for long class name
-     *
-     * It is alias for registerClassAlias() function
-     *
-     * @param string|array $alias
-     * @return void
-     */
-    public static function registerClassAlias($alias)
-    {
-        return registerClassAlias($alias);
-    }
 }
 
 
@@ -95,7 +67,10 @@ class Akt
 // ===================================================== */
 
 /**
- * Alias for Akt::module()
+ * Loads module by its name and instantiate it
+ *
+ * By default instantiate each module once (Singleton)
+ * If you need new instance of some module specify $instance parameter
  *
  * @param string $name
  * @param string $instance
@@ -103,30 +78,109 @@ class Akt
  */
 function module($name, $instance = 'default')
 {
-    return Akt::module($name, $instance);
+    //
 }
 
 /**
+ * Execute task by its name
  *
+ * @param string $name
+ * @param array $params
+ * @return mixed
+ */
+function task($name, $params = array())
+{
+    $taskfunc = 'task_' . $name;
+    $taskClass = ucfirst($name) . 'Task';
+
+    if (!function_exists($taskfunc) && !class_exists($taskClass, false)) {
+        // load task file
+    }
+    
+    if (function_exists($taskfunc)) {
+        return call_user_func_array($taskfunc, $params);
+    }
+    elseif (class_exists($taskClass, false)) {
+        $class = new $taskClass();
+        //$class->setOptions($params);
+        return $class->execute();
+    }
+
+    throw new Akt_Exception("Task '$name' not found");
+}
+
+/**
+ * Execute task if it wasn't executed before
+ *
+ * If task needs parameters, this task must be provided by array:
+ *   array('taskName', array(param1, param2, ...))
+ *
+ * You can also specify bool parameter 'force' in "task array",
+ * which means that task must be run even if it has been already executed
+ * By default this parameter is false:
+ *     array('taskName', array(param1, param2, ...), true)
+ *     or array('taskName', true), if no parameters needed
+ * In this case this is the same as task() function calling,
+ * but useful on multiple task depending:
+ *     depends('task1', array('task2', true), 'task3');
+ *       instead of
+ *     depends('task1');
+ *     task('task2');
+ *     depends('task3');
+ *
+ *
+ * @staticvar array $executedTasks
+ * @return void
  */
 function depends()
 {
+    static $executedTasks = array();
+
+    $tasks = array();
+    $defaultParams = array();
+    $defaultForce = false;
+
     $args = func_get_args();
+    $argc = count($args);
 
-    $resultStack = array();
+    if (!$argc) {
+        return;
+    }
 
-    foreach ($args as $task)
+    foreach ($args as $arg)
     {
-        $taskfunc = 'task_' . $task;
-        $taskClass = ucfirst($task) . 'Task';
-        if (function_exists($taskfunc)) {
-            $taskResult = $taskfunc($resultStack);
+        if (is_string($arg)) {
+            $arg = array($arg);
         }
-        elseif (class_exists($taskClass, false)) {
-            $class = new $taskClass();
-            $taskResult = $class->execute($resultStack);
+        elseif (!is_array($arg)) {
+            continue;
         }
-        $resultStack[] = $taskResult;
+        $taskName = array_shift($arg);
+        $taskParams = $arg ? array_shift($arg) : $defaultParams;
+        if (is_bool($taskParams)) {
+            $taskForce = $taskParams;
+            $taskParams = $defaultParams;
+        }
+        else {
+            $taskForce = $arg ? array_shift($arg) : $defaultForce;
+        }
+        $tasks[] = array(
+            'name' => $taskName,
+            'params' => $taskParams,
+            'force' => $taskForce,
+        );
+    }
+
+    foreach ($tasks as $task)
+    {
+        $alreadyExecuted = in_array(strtolower($task['name']), $executedTasks);
+        if (!$alreadyExecuted || $task['force'])
+        {
+            task($task['name'], $task['params']);
+            if (!$alreadyExecuted) {
+                $executedTasks[] = strtolower($task['name']);
+            }
+        }
     }
 }
 
